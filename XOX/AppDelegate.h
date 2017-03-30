c4ff1cc9e43b749a54d9b912cea9f61ae5ae0416
berk <berk@berks-imac.local>
2017-03-30 13:38:46 -0400

    Initial Commit

---
+++ XOX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XOX
+//
+//  Created by berk on 3/30/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XOX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XOX
+//
+//  Created by berk on 3/30/17.
+//  Copyright © 2017 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
